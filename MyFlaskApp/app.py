59273150156e39f4cd8a66c42b8340c95b0cb3cf
Nadavos <nadav.ostrowsky@biocatch.com>
2018-07-18 15:06:38 +0300

    create now in restful

--- MyFlaskApp/app.py
+++ MyFlaskApp/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, redirect, jsonify
-from flask_restful import Api, Resource, reqparse, fields, marshal
+from flask import Flask, request, jsonify
+from flask_restful import Api
 from pokemon import new_pokemon, search
-from wtforms import Form, StringField, TextAreaField, validators
 
 
 app = Flask(__name__)
@@ -10,61 +9,12 @@ api = Api(app)
 cache = {}
 
 
-# class Search(Resource):
-#     prefix_str = StringField('', [validators.Length(min=1)])
-#
-#     def __init__(self):
-#         self.reqparse = reqparse.RequestParser()
-#
-#
-#
-# class NewPokemon(Form):
-#     pokemon_json_string = TextAreaField('Enter Pokemon JSON')
-#
-#
-#
-# @app.route('/')
-# def hello():
-#     return render_template('pokemon_home.html')
-#
-#
-# @app.route('/add_pokemon', methods=['GET', 'POST'])
-# def add_pokemon():
-#     global cache
-#     form = NewPokemon(request.form)
-#     if request.method == 'POST' and form.validate():
-#         pokemon_obj = form.pokemon_json_string.data
-#         res = new_pokemon(pokemon_obj)
-#         if res:
-#             cache = {}
-#             return render_template('success.html')
-#         return render_template('add_pokemon.html', form=form)
-#     return render_template('add_pokemon.html', form=form)
-#
-#
-# @app.route('/search_prefix', methods=['GET', 'POST'])
-# def search_prefix():
-#     form = Search(request.form)
-#     if request.method == 'POST' and form.validate():
-#         prefix = form.prefix_str.data
-#         if cache.get(prefix):
-#             res = cache[prefix]
-#         else:
-#             res = search(prefix)
-#             cache[prefix] = res
-#         return render_template('search_prefix.html', form=form, pokemons=res)
-#     return render_template('search_prefix.html', form=form)
+@app.route('/Create a new Pokemon', methods=['Put'])
+def add_pokemon():
+    pokemon_json = request.get_json()
+    new_pokemon(pokemon_json)
+    return jsonify({'Created': pokemon_json['name']})
 
-# class Pokemon(Resource):
-#     def __init__(self):
-#         self.parser = reqparse.RequestParser()
-#         self.parser.add_argument('name', type=str, location='json', required=True)
-
-
-@app.route('/Create a new Pokemon/<string:pokemon_json>')
-def add_pokemon(pokemon_json):
-    # new_pokemon(pokemon_json)
-    return pokemon_json
 
 @app.route('/Auto-complete/<string:prefix>')
 def search_prefix(prefix):
--- MyFlaskApp/pokemon.py
+++ MyFlaskApp/pokemon.py
@@ -8,34 +8,8 @@ import requests
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 
-def validate_pokemon_json(json_string):
-    types = ["ELECTRIC", "GROUND", "FIRE", "WATER", "WIND", "PSYCHIC", "GRASS", 'NORMAL', 'ROCK', 'POISON', 'BUG', 'ICE', 'GHOST', 'DRAGON']
-    try:
-        json_obj = json.loads(json_string)
-    except ValueError as e:
-        print e
-        return False
-    if not isinstance(json_obj.get('name'), unicode):
-        return False
-    if not isinstance(json_obj.get('level'), int) or json_obj['level'] < 0:
-        return False
-    if not isinstance(json_obj.get('skills'), list):
-        return False
-    if not isinstance(json_obj.get('type'), unicode) or json_obj['type'] not in types:
-        return False
-    if not isinstance(json_obj.get('nickname'), unicode):
-        return False
-    if not isinstance(json_obj.get('pokadex_id'), int) or json_obj['pokadex_id'] < 0:
-        return False
-    return True
-
-
 def new_pokemon(pokemon_json):
-    if validate_pokemon_json(pokemon_json):
-        pokemon_json_object = json.loads(pokemon_json)
-        es.index(index='exercise', doc_type='pokemon', id=pokemon_json_object['pokadex_id'], body=pokemon_json_object)
-        return True
-    return False
+    es.index(index='exercise', doc_type='pokemon', id=pokemon_json['pokadex_id'], body=pokemon_json)
 
 
 def search(string):
